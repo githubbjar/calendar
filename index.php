d4911bcec893a5b8d8ee58a8326608da6419b148
janQuart <janquart@fsj.org>
2021-08-06 09:44:49 -0500

    added css page, filled in head details

--- index.php
+++ index.php
@@ -3,12 +3,12 @@
 <head>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta charset="utf-8">
-<title></title>
-<meta name="description" content="">
-<meta name="author" content="">
+<title>Anno Domini 2022</title>
+<meta name="description" content="Anno Domini 2022: The St. James Calendar of the Christian Year">
+<meta name="author" content="The Fellowship of St. James">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-<link rel="stylesheet" href="">
+<link rel="stylesheet" href="custom.css">
 <!--[if lt IE 9]>
 <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
