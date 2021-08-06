cbf929fd6f6a689c8f3fb6ec6ccd51dd59d8cf20
janQuart <janquart@fsj.org>
2021-08-06 14:01:11 -0500

    first draft

---
+++ index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta charset="utf-8">
+<title>Anno Domini 2022</title>
+<meta name="description" content="Anno Domini 2022: The St. James Calendar of the Christian Year">
+<meta name="author" content="The Fellowship of St. James">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<link rel="stylesheet" href="custom.css">
+<!--[if lt IE 9]>
+<script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<link rel="shortcut icon" href="">
+</head>
+<body>
+	<nav id="scrolspy" class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+	  <div class="container-fluid">
+	    <a class="navbar-brand" href="#">Anno Domini 2022</a>
+	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+	      <span class="navbar-toggler-icon"></span>
+	    </button>
+	    <div class="collapse navbar-collapse" id="navbarCollapse">
+	      <ul class="navbar-nav me-auto mb-2 mb-md-0">
+	        <li class="nav-item">
+	          <a class="nav-link" href="#scrollspyHeading1">About</a>
+	        </li>
+	        <li class="nav-item">
+	          <a class="nav-link" href="#scrollspyHeading2">See the Calendar</a>
+	        </li>
+	      	<li class="nav-item">
+	          <a class="nav-link" href="#">ORDER TODAY Only $15.95</a>
+	        </li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
+<div data-bs-spy="scroll" data-bs-target="#scrollspy" data-bs-offset="0" class="scrollspy-example" tabindex="0">
+	<div class="row" id="first-row">
+		<div class="col-sm-1"></div>
+		<div class="col-sm-10">
+			<img src="angel.png" width="100%" />
+		</div>
+		<div class="col-sm-1"></div>
+	</div>
+
+	<div class="row" id="second-row">
+		<div class="col-sm-3"></div>
+		<div class="col-sm-6">
+			<img src="logo-only.png" width="100%" />
+		</div>
+		<div class="col-sm-3"></div>
+	</div>
+
+	<div class="row" id="third-row">
+		<div class="col-sm-12">
+			<p>The St. James Calendar of <br />
+			the Christian Year</p>
+		</div>
+	</div>
+
+	<div class="row" id="fourth-row">
+		<div class="col-sm-2" id="scrollspyHeading1"></div>
+		<div class="col-sm-4">
+			
+			<p><span class="dropcap">T</span>he St. James Calendar of the Christian Year reflects the common, ecumenical heritage that Christians share in the feast days and saints of the early, undivided Church, as it is expressed in the calendars of the Western and Eastern churches.</p>
+
+			<p>Pride of place in any Christian calendar, of course, belongs to the commemorations of the life of Our Lord Jesus Christ—the Annunciation, Nativity, Baptism, Triumphal Entry, Crucifixion, -Resurrection, and Ascension, among others.</p>
+
+			<p>But unlike many Christian calendars, the St. James Calendar also lists saints of both the Old and New -Testaments—prophets, apostles, evangelists and many -others—on their dates as -commemorated in the Eastern and Western churches.</p>
+
+			<p>Furthermore, and uniquely, the St. James Calendar lists saints from antiquity who are commemorated on the same dates in both Eastern and Western calendars. There are hundreds of such saints shared by East and West throughout the year.</p>
+
+		</div>
+		<div class="col-sm-4">
+			
+			<p>This unique calendar is thus an -expression and daily reminder of the “communion of the saints” to which all Christians belong. It includes prophets, apostles, martyrs, missionaries, monks, children, married and celibate men and women, -peasants and kings, preachers, bishops, and pastors—all -members of the mystical Body of Christ, a “great cloud of witnesses.” </p>
+
+			<p>The St. James Calendar is a modest attempt to present a rich treasure of our Christian heritage, biblically grounded, integral to the life of the early Church, and preserved in the liturgical traditions of various churches. It is hoped that this calendar will encourage a deeper appreciation of the Christian year and its message of salvation in the Lord Jesus Christ.</p>
+
+ 			<p>Engravings for the calendar months are selected in part to correspond to the Bible readings in the St. James Daily Devotional Guide for the Christian Year, edited by Patrick Henry Reardon, who also selected the monthly quotations from the church fathers to accompany the calendar illustrations. </p>
+
+		</div>
+		<div class="col-sm-1"></div>
+	</div>
+
+	<div class="row" id="fifth-row">
+		<div class="col-sm-12">
+			<p>The St. James Calendar is:</p>
+		</div>
+	</div>
+
+	<div class="row" id="sixth-row">
+
+		<div class="col-sm-1"></div>
+		<div class="col-sm-3" id="margin"><span class="red">&#8226; </span>An 11 x 17 wall calendar with notes, quotations from the Church Fathers, and 13 classic biblical engravings</div>
+		<div class="col-sm-4" id="margin"><span class="red">&#8226; </span>Includes more than 1,000 saints from antiquity commemorated on the same dates in both Eastern and Western calendars, an expression of the unity of our Faith</div>
+		<div class="col-sm-3" id="margin"><span class="red">&#8226; </span>A daily reminder of the communion of the saints for your home or office</div>
+		<div class="col-sm-1"></div>
+
+	</div>
+
+	<div class="row" id="seventh-row">
+		<div class="col-sm-2"></div>
+		<div class="col-sm-3" id="thumbnails">
+			<img src="holyfam.jpg" width="100%" />
+			<img src="Elijah.jpg" width="100%" />
+			<img src="woman.jpg" width="100%" />
+		</div>
+		<div class="col-sm-6" id="scrollspyHeading2">
+			<a href="july2022.jpg"><img src="july2022.jpg" class="calendar" /></a>
+		</div>
+		<div class="col-sm-1"></div>
+	</div>
+
+	<div class="row">
+		<div class="col-sm-3"></div>
+		<div class="col-sm-6" id="black" href="#">Order today! Only $15.95 each</div>
+		<div class="col-sm-3"></div>
+	</div>
+
+	<div class="row" id="eighth-row">
+		<div class="col-sm-12">
+			<p>Copyright © 2021 The Fellowship of St. James, Chicago, IL 60641<br />The content and arrangements of this calendar are copyrighted and may not be reproduced in any form without written permission of the publisher. All rights reserved.</p>
+		</div>
+	</div>
+
+</div>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
+</body>
+</html>
